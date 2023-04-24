bf8fed67d3c189c8fd3f7af6cfa65a6a09ea58a9
MuriGbada <muri.gbadamosi@gmail.com>
2023-04-24 18:27:42 +0100

    deleted

--- Front-End_Developer/Course 2 Programming with JavaScript/Math_Method/object.js
+++
@@ -1,9 +0,0 @@
-// create a empty object literal with variable favCar as
-// an empty list, the add prperty:value pairs using dot notation
-
-var favCar = {};
-
-favCar.color = 'blue';
-favCar.convertible = true;
-
-console.log(favCar);
